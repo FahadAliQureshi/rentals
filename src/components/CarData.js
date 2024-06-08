3a6060a10b56bb4723d1db72f5d8a61dcfe31c17
Fahad Ali <fahad.ali@appinsnap.com>
2024-06-08 16:11:25 +0500

    Initial commit

---
+++ src/components/CarData.js
@@ -0,0 +1,93 @@
+import HondaCivic from "../images/cars-big/HondaCivic.jpg";
+import SuzukiSwift from "../images/cars-big/SuzukiSwift.jpg";
+import ToyotaCorolla from "../images/cars-big/ToyotaCorolla.jpg";
+import SuzukiAlto from "../images/cars-big/SuzukiAlto.jpg";
+import HondaCity from "../images/cars-big/HondaCity.jpg";
+import ToyotaFortuner from "../images/cars-big/ToyotaFortuner.jpg";
+
+export const CAR_DATA = [
+  [
+    {
+      name: "Honda Civic",
+      price: "40",
+      img: HondaCivic,
+      model: "Civic",
+      mark: "Honda",
+      year: "2020",
+      doors: "4/5",
+      air: "Yes",
+      transmission: "Automatic",
+      fuel: "Petrol",
+    },
+  ],
+  [
+    {
+      name: "Suzuki Swift",
+      price: "30",
+      img: SuzukiSwift,
+      model: "Swift",
+      mark: "Suzuki",
+      year: "2018",
+      doors: "4/5",
+      air: "Yes",
+      transmission: "Manual",
+      fuel: "Petrol",
+    },
+  ],
+  [
+    {
+      name: "Toyota Corolla",
+      price: "35",
+      img: ToyotaCorolla,
+      model: "Corolla",
+      mark: "Toyota",
+      year: "2019",
+      doors: "4/5",
+      air: "Yes",
+      transmission: "Automatic",
+      fuel: "Petrol",
+    },
+  ],
+  [
+    {
+      name: "Suzuki Alto",
+      price: "20",
+      img: SuzukiAlto,
+      model: "Alto",
+      mark: "Suzuki",
+      year: "2021",
+      doors: "4/5",
+      air: "Yes",
+      transmission: "Manual",
+      fuel: "Petrol",
+    },
+  ],
+  [
+    {
+      name: "Honda City",
+      price: "35",
+      img: HondaCity,
+      model: "City",
+      mark: "Honda",
+      year: "2017",
+      doors: "4/5",
+      air: "Yes",
+      transmission: "Automatic",
+      fuel: "Petrol",
+    },
+  ],
+  [
+    {
+      name: "Toyota Fortuner",
+      price: "60",
+      img: ToyotaFortuner,
+      model: "Fortuner",
+      mark: "Toyota",
+      year: "2021",
+      doors: "4/5",
+      air: "Yes",
+      transmission: "Automatic",
+      fuel: "Diesel",
+    },
+  ],
+];
